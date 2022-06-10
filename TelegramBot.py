1326f5e50b3bada2b3ef3d1f6e6ceef2ee04ce79
FarhatN <farhat_ink@mail.ru>
2022-06-10 09:17:18 +0500

    Добавление функции для команды help

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,12 +1,26 @@
 import telebot
 from decouple import config
 
+
 token = config('token')
 
+
 bot = telebot.TeleBot(token)
+
+
+HELP = '''
+/help  - вывести список доступных команд
+'''
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, HELP)
+
 
 @bot.message_handler(content_types=["text"])
 def echo(message):
     bot.send_message(message.chat.id, message.text)
+
 
 bot.polling(none_stop=True)
